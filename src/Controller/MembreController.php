b32530660e050ef732ce5747e42b7b5882b0f8d9
Ashaï <ashaijasaron@gmail.com>
2026-01-03 15:13:29 +0100

    feat: création des fonctionnalités pour lister les utilisateurs et en afficher

--- src/Controller/MembreController.php
+++ src/Controller/MembreController.php
@@ -2,17 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Membre;
+use App\Repository\MembreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
-final class MembreController extends AbstractController
+class MembreController extends AbstractController
 {
-    #[Route('/membre', name: 'app_membre')]
-    public function index(): Response
+    public function index(MembreRepository $membreRepository): JsonResponse
     {
-        return $this->render('membre/index.html.twig', [
-            'controller_name' => 'MembreController',
-        ]);
+        $membres = $membreRepository->findAll();
+
+        $data = array_map( function (Membre $membre) {
+            return [
+                'id' => $membre->getId(),
+                'firstName' => $membre->getName(),
+                'lastName' => $membre->getLastName(),
+                'email' => $membre->getEmail(),
+                'roles' => $membre->getRoles(),
+            ];
+        }, $membres);
+
+        return new JsonResponse($data, JsonResponse::HTTP_OK);
+    }
+
+    public function show(Membre $membre): JsonResponse
+    {
+        $data = [
+                'id' => $membre->getId(),
+                'firstName' => $membre->getName(),
+                'lastName' => $membre->getLastName(),
+                'email' => $membre->getEmail(),
+                'roles' => $membre->getRoles(),
+            ];
+            return new JsonResponse($data, JsonResponse::HTTP_OK);
     }
 }
