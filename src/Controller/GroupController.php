4b316153ca735f408fb894215ad08b5df030cd46
Ashaï <ashaijasaron@gmail.com>
2026-02-11 11:23:07 +0100

    <feat>: ajout de la fonction show pour afficher un groupe

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Group;
 use App\Repository\GroupRepository;
+use Doctrine\ORM\Query\Expr\Func;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
@@ -24,6 +25,18 @@ class GroupController extends AbstractController
                 'members' => $group->getMembers(),
             ];
         }, $groups);
+        return new JsonResponse($data, JsonResponse::HTTP_OK);
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/api/groupes/{id}', methods: ['GET'], name: 'app_group_show')]
+    public function show(Group $group): JsonResponse
+    {
+        $data = [
+            'id' => $group->getId(),
+            'name' => $group->getName(),
+            'members' => $group->getMembers(),
+        ];
         return new JsonResponse($data, JsonResponse::HTTP_OK);
     }
 }
